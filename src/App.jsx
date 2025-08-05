befafee4989cbe66379d8c58d3734dcd1f5d35c1
hassaan mohamed <hassaanmohamed333@gmail.com>
2025-08-05 11:03:15 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       path: "/", 
       element: <Layout />, 
       children: [
-        { path: "", element: <Home /> },
+        { index: true, element: <Home /> },
         { path: "about", element: <About /> },
         { path: "portfolio", element: <Portfolio /> },
         { path: "contact", element: <Contact /> },
